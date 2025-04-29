08fd6a58e480db8a85e2340413ccb7d4bb7eb837
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-04-29 20:29:48 +0200

    add functions to delete and post new posts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,13 @@ import { Post } from '../components/Post';
 import '../global.css';
 import './index.css';
 
+const loggedInUserId = 4;
+const response = await fetch(
+  `http://localhost:4000/api/users/${loggedInUserId}`,
+);
+const json = await response.json();
+const loggedInUser = json.data;
+
 const fetchPosts = async () => {
   const response = await fetch(`http://localhost:4000/api/posts`);
   const json = await response.json();
@@ -14,16 +21,51 @@ const posts = await fetchPosts();
 document.querySelector('#root').innerHTML = render(
   <div className="container">
     <h1>The Chirp</h1>
+    <p>Prihlasen jako {loggedInUser.name}</p>
     <form className="post-form">
       <p>Co máte na srdci?</p>
       <textarea placeholder="Napište něco..." className="post-input"></textarea>
       <button type="submit">Publikovat</button>
     </form>
-    
+
     <div>
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </div>
-  </div>
+  </div>,
 );
+
+const deleteButtons = document.querySelectorAll('.delete-btn');
+deleteButtons.forEach((button) => {
+  button.addEventListener('click', async () => {
+    const postId = button.dataset.id;
+    await fetch(`http://localhost:4000/api/posts/${postId}`, {
+      method: 'DELETE',
+    });
+    window.location.reload();
+  });
+});
+
+const form = document.querySelector('.post-form');
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const input = document.querySelector('.post-input');
+  const text = input.value;
+  await fetch(`http://localhost:4000/api/posts`, {
+    headers: {
+      'Content-type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      userId: loggedInUser.id,
+      userName: loggedInUser.name,
+      userHandle: loggedInUser.handle,
+      userAvatar: loggedInUser.avatar,
+      text: text,
+      likes: 0,
+    }),
+  });
+  window.location.reload();
+});
+
